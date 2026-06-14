6a343062e7213ad6b7be8393bc28c2a83d1ab9a5
Conrad008 <kipngeno7180@gmail.com>
2026-06-14 19:08:07 +0300

    test: Configure Jest test environment and mock localStorage

--- script.test.js
+++ script.test.js
@@ -1,2 +1,18 @@
 const { Expense, ExpenseTracker } = require('./script.js');
 
+describe('Ledgerly Expense Tracker Unit Tests', () => {
+    let tracker;
+
+    beforeAll(() => {
+        global.localStorage = {
+            getItem: jest.fn(() => null),
+            setItem: jest.fn(),
+            clear: jest.fn()
+        };
+    });
+
+    beforeEach(() => {
+        tracker = new ExpenseTracker();
+        jest.clearAllMocks();
+    });
+});
